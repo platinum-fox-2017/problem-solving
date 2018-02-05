84e53c5379e8198cd26b2db78702a3f8f0099f75
FTeddy <ainsophoure@gmail.com>
2018-02-05 16:27:17 +0700

    changed into a function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 name: Tono, freq: 2
 name: Anton, freq: 4
 name: Budi, freq: 5
+visitor = [ ['Tono', 2], ['Anton', 4], ['Budi', 5] ]
 start tanggal startDate:7 , closed every multiple of 5 days
 
 set variable month = 31 days
@@ -21,37 +22,42 @@ set variable startDate as 7
     // turn visiting to string with ', ' separator
     // after adding visiting members, add it to output
 */
-var month = 31;
-var startDate = 7;
-var visiting = [];
 
 
-for (var date = startDate; date <= month; date++) {
-  visiting = [];
-  var output = 'Tanggal ' + date + ': ';
-  if(date === 7){
-    visiting.push('Tono');
-    visiting.push('Anton');
-    visiting.push('Budi');
-    visiting = visiting.join(', ');
-    console.log(output + visiting);
-  } else {
-    // if the date is divisible by 5 then log output + 'Tempat Fitness Tutup'
-    if(date % 5 === 0) {
-      console.log(output + 'Tempat Fitness Tutup');
-    } else {
-      if((date-startDate) % 2 === 0){
-        visiting.push('Tono');
-      }
-      if((date-startDate) % 4 === 0){
-        visiting.push('Anton');
-      }
-      if((date-startDate) % 5 === 0){
-        visiting.push('Budi');
+function visitLog(visitor){
+  var month = 31;
+  var startDate = 7;
+  var visiting = [];
+
+  for (var date = startDate; date <= month; date++) {
+    visiting = [];
+    var output = 'Tanggal ' + date + ': ';
+    if(date === 7){
+      // loop through visitor
+      for (var v = 0; v < visitor.length; v++) {
+        var visitorName = visitor[v][0];
+        visiting.push(visitorName);
       }
       visiting = visiting.join(', ');
       console.log(output + visiting);
+    } else {
+      // if the date is divisible by 5 then log output + 'Tempat Fitness Tutup'
+      if(date % 5 === 0) {
+        console.log(output + 'Tempat Fitness Tutup');
+      } else {
+        // loop through visitor
+        for (var r = 0; r < visitor.length; r++) {
+          var visitDays = visitor[r][1];
+          visitorName = visitor[r][0];
+          if((date-startDate) % visitDays === 0){
+            visiting.push(visitorName);
+          }
+        }
+        visiting = visiting.join(', ');
+        console.log(output + visiting);
+      }
     }
   }
-
 }
+
+console.log(visitLog([ ['Tono', 2], ['Anton', 4], ['Budi', 5] ]));
