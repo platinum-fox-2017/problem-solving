bfaf350ffeb8420d09a65d4af69e3d39c1c52dae
Herby Herado <herby,herado@gmail.com>
2018-02-07 18:36:27 +0700

    code refactoring

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
 function fitness(){
-    day = [];
-    names = [];
+    var countTono = 7;
+    var countAnton = 7;
+    var countBudi = 7;
 
-    // buatlah template tanggal dari tanggal 7 sampai 31
-    for(i = 7; i <= 31; i++){
-        // tempat fitness tutup setiap tangga kelipatan 5
+    for(var i = 7; i <= 31; i++){
+        var str  = `Tanggal ${i}:`;
+        var names = [];
+        var tutup = ' Tempat Fitness Tutup';
+        
         if (i % 5 == 0){
-            day.push(`Tanggal ${i}: Tempat Fitness Tutup`)
-        // tampilan untuk jadwal kosong
-        } else {
-            day.push(`Tanggal ${i}:`)
+            names.push(tutup);
         }
-    }
-    // console.log(day.length)
-
-    // push nama pada array 'names' berdasarkan hari-harinya
-    for (i = 0; i <= day.length; i++){
-        names.push([])
-        if (i % 2 == 0){
-            names[i].push(' Tono');
+        if (countTono === i){
+            countTono += 2;
+            if (names.indexOf(tutup) == -1){
+                names.push(' Tono');
+            }
         }
-        if (i % 4 == 0){
-            names[i].push(' Anton')
+        if (countAnton === i){
+            countAnton += 4;
+            if (names.indexOf(tutup) == -1){
+                names.push(' Anton');
+            }
         }
-        if (i % 5 == 0){
-            names[i].push(' Budi')
+        if (countBudi === i){
+            countBudi += 5;
+            if (names.indexOf(tutup) == -1){
+                names.push(' Budi');
+            }
         }
-    }
-
-    // modify array 'day', bila belum ada 'tempat fitness tutup' akan .concat array 'names' kepada array 'day'
-    for (j = 0; j < day.length; j++){
-        if (day[j].length < 32){
-            console.log(day[j].concat(names[j]))
-        } else {
-            console.log(day[j])
-        }
+        names = names.join(',');
+        console.log(str.concat(names));
     }
 }
-
 fitness();
